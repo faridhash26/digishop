6888e469d7af77ef0b2fa965db2ee6c82b2ef336
farid hash <kingfarid76@gmail.com>
2021-01-02 12:41:05 +0330

    final my project

--- backend/middleware/errorMidleware.js
+++ backend/middleware/errorMidleware.js
@@ -5,13 +5,13 @@ const notfound = (req, res, next) => {
 };
 
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode
+  res.status(statusCode)
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
-};
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  })
+}
 
 module.exports = {
   notfound,
